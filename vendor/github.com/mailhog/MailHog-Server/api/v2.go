590010b8ac0d28459b0094c8c8afd2e1bad621bf
azagirov <azatps@gmail.com>
2022-03-04 13:50:39 +0300

    try add sort

--- vendor/github.com/mailhog/MailHog-Server/api/v2.go
+++ vendor/github.com/mailhog/MailHog-Server/api/v2.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"net/http"
 	"strconv"
-	"sort"
 
 	"github.com/gorilla/pat"
 	"github.com/ian-kent/go-log/log"
@@ -115,11 +114,6 @@ func (apiv2 *APIv2) messages(w http.ResponseWriter, req *http.Request) {
 	res.Start = start
 	res.Items = []data.Message(*messages)
 	res.Total = apiv2.config.Storage.Count()
-
-	log.Println("Attempting sort")
-	sort.Slice(res.Items, func(i, j int) bool{
-		return res.Items[i].Created.After(res.Items[j].Created)
-	})
 
 	bytes, _ := json.Marshal(res)
 	w.Header().Add("Content-Type", "text/json")
