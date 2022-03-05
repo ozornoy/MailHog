834b7eec3fbc60cdc8d4dc3b141880d0287e37c1
ozornoy <70505353+ozornoy@users.noreply.github.com>
2022-03-05 13:18:58 +0500

    fix

--- vendor/github.com/mailhog/storage/maildir.go
+++ vendor/github.com/mailhog/storage/maildir.go
@@ -1,4 +1,5 @@
 package storage
+
 import (
 	"errors"
 	"io/ioutil"
@@ -6,8 +7,8 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
-
-        "sort"
+	
+	"sort"
 	"github.com/mailhog/data"
 )
 
@@ -15,6 +16,7 @@ import (
 type Maildir struct {
 	Path string
 }
+
 // CreateMaildir creates a new maildir storage backend
 func CreateMaildir(path string) *Maildir {
 	if len(path) == 0 {
@@ -35,6 +37,7 @@ func CreateMaildir(path string) *Maildir {
 		Path: path,
 	}
 }
+
 // Store stores a message and returns its storage ID
 func (maildir *Maildir) Store(m *data.Message) (string, error) {
 	b, err := ioutil.ReadAll(m.Raw.Bytes())
@@ -44,6 +47,7 @@ func (maildir *Maildir) Store(m *data.Message) (string, error) {
 	err = ioutil.WriteFile(filepath.Join(maildir.Path, string(m.ID)), b, 0660)
 	return string(m.ID), err
 }
+
 // Count returns the number of stored messages
 func (maildir *Maildir) Count() int {
 	// FIXME may be wrong, ../. ?
@@ -56,23 +60,29 @@ func (maildir *Maildir) Count() int {
 	n, _ := dir.Readdirnames(0)
 	return len(n)
 }
+
 // Search finds messages matching the query
 func (maildir *Maildir) Search(kind, query string, start, limit int) (*data.Messages, int, error) {
 	query = strings.ToLower(query)
 	var filteredMessages = make([]data.Message, 0)
+
 	var matched int
+
 	err := filepath.Walk(maildir.Path, func(path string, info os.FileInfo, err error) error {
 		if limit > 0 && len(filteredMessages) >= limit {
 			return errors.New("reached limit")
 		}
+
 		if info.IsDir() {
 			return nil
 		}
+
 		msg, err := maildir.Load(info.Name())
 		if err != nil {
 			log.Println(err)
 			return nil
 		}
+
 		switch kind {
 		case "to":
 			for _, t := range msg.To {
@@ -102,33 +112,40 @@ func (maildir *Maildir) Search(kind, query string, start, limit int) (*data.Mess
 				filteredMessages = append(filteredMessages, *msg)
 			}
 		}
+
 		return nil
 	})
+
 	if err != nil {
 		log.Println(err)
 	}
+
 	msgs := data.Messages(filteredMessages)
 	return &msgs, len(filteredMessages), nil
 }
+
 // List lists stored messages by index
 func (maildir *Maildir) List(start, limit int) (*data.Messages, error) {
 	log.Println("Listing messages in", maildir.Path)
 	messages := make([]data.Message, 0)
+
 	dir, err := os.Open(maildir.Path)
 	if err != nil {
 		return nil, err
 	}
 	defer dir.Close()
+
 	n, err := dir.Readdir(0)
 	if err != nil {
 		return nil, err
 	}
+
 	for _, fileinfo := range n {
 		b, err := ioutil.ReadFile(filepath.Join(maildir.Path, fileinfo.Name()))
 		if err != nil {
 			return nil, err
 		}
-                sort.Slice(n, func(i, j int) bool {
+		sort.Slice(n, func(i, j int) bool {
                   return  n[i].ModTime().After(n[j].ModTime())
                 })
 
@@ -139,14 +156,17 @@ func (maildir *Maildir) List(start, limit int) (*data.Messages, error) {
 		m.Created = fileinfo.ModTime()
 		messages = append(messages, m)
 	}
+
 	log.Printf("Found %d messages", len(messages))
 	msgs := data.Messages(messages)
 	return &msgs, nil
 }
+
 // DeleteOne deletes an individual message by storage ID
 func (maildir *Maildir) DeleteOne(id string) error {
 	return os.Remove(filepath.Join(maildir.Path, id))
 }
+
 // DeleteAll deletes all in memory messages
 func (maildir *Maildir) DeleteAll() error {
 	err := os.RemoveAll(maildir.Path)
@@ -155,6 +175,7 @@ func (maildir *Maildir) DeleteAll() error {
 	}
 	return os.Mkdir(maildir.Path, 0770)
 }
+
 // Load returns an individual message by storage ID
 func (maildir *Maildir) Load(id string) (*data.Message, error) {
 	b, err := ioutil.ReadFile(filepath.Join(maildir.Path, id))
